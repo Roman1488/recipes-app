795578c258945452d693c218d222aee5dd5cd8d5
roman1488 <roman.zvyazok@eleks.com>
2019-11-12 14:10:21 +0200

    add responsive & ingredients button

--- src/App.js
+++ src/App.js
@@ -32,11 +32,9 @@ const App = () => {
   return(
       <div className="container">
           <form className="recipes-form" onSubmit={getSearch}>
-            <div className="form-row justify-content-center">
-                <div className="form-group">
-                    <input value={search} type="search" className="form-control" aria-describedby="search" onChange={updateSearch} />
-                </div>
-                <div className="form-group">
+            <div className="input-group mb-3">
+                <input value={search} type="search" placeholder="Search..." className="form-control" aria-label="Search" aria-describedby="Search" onChange={updateSearch} />
+                <div className="input-group-append">
                     <button type="submit" className="btn btn-outline-primary">Search</button>
                 </div>
             </div>
@@ -45,7 +43,7 @@ const App = () => {
               <div className="container">
                   <div className="row">
               {recipes.map(recipe => (
-                  <div key={recipe.recipe.label} className="col-4">
+                  <div key={recipe.recipe.label} className="col-12 col-md-6 col-xl-4">
                       <Recipe
                           title={recipe.recipe.label}
                           calories={recipe.recipe.calories.toFixed(2)}
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Recipe = ({title, calories, image, ingredients}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const $ingredients = isOpen && <ul className="recipes__ingredients">
+                        {ingredients.map((ingredient, index) => (
+                            <li className="card-text" key={index}>{ingredient.text}</li>
+                        ))}
+                        </ul>;
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
     return(
         <div className="card bg-light recipes-list__item">
             <img className="card-img-top" src={image} alt={title}/>
             <h3 className="card-header">{title}</h3>
             <div className="card-body">
                 <p className="card-title">Calories: {calories}</p>
-                <ul>
-                    {ingredients.map((ingredient, index) => (
-                        <li className="card-text" key={index}>{ingredient.text}</li>
-                    ))}
-                </ul>
+                <button className="btn btn-primary" onClick={handleClick}>
+                    { isOpen ? 'Hide ingredients' : 'Show ingredients' }
+                </button>
+                {$ingredients}
             </div>
         </div>
     );
